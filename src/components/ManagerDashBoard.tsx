00a44f070220b9426a1e2b9b8b2e58b0e101de18
Vy Truong <trmninhvy@gmail.com>
2024-10-16 13:36:51 -0600

    removed components

--- src/components/ManagerDashBoard.tsx
+++ src/components/ManagerDashBoard.tsx
@@ -3,15 +3,14 @@
 import React from 'react';
 import { Container, Typography, Button, Box } from '@mui/material';
 import { styled } from '@mui/system';
-import SchedulerContainer from '../schedules/SchedulerContainer';
-import { Component } from '../schedules/bitnotScheduler';
+import { ManagerViewScheduler } from '../schedules/ManagerViewScheduler';
 const ManagerDashBoard: React.FC = () => { 
     return (
         <Typography> 
             This Is Employee DashBoard: 
                 Available Page: 
                 <Box>
-                    <Component />
+                    <ManagerViewScheduler />
                 </Box>
                
         </Typography>
--- src/schedules/HomePage.tsx
+++ src/schedules/HomePage.tsx
@@ -1,58 +1,58 @@
-import React, { useState, useEffect } from "react";
-import { Box, Paper, Typography, Button } from "@mui/material";
+// import React, { useState, useEffect } from "react";
+// import { Box, Paper, Typography, Button } from "@mui/material";
 
-import SignUp from "../userAuth/SignUp";
-import SignOut from "../userAuth/SignOut";
-import { useUserStore } from "../stores/useUserStore";
-import { onAuthStateChanged } from "firebase/auth";
-import { auth } from "../userAuth/firebase";
+// import SignUp from "../userAuth/SignUp";
+// import SignOut from "../userAuth/SignOut";
+// import { useUserStore } from "../stores/useUserStore";
+// import { onAuthStateChanged } from "firebase/auth";
+// import { auth } from "../userAuth/firebase";
 
-import SignInSide from "../userAuth/ManagerSignIn/SignInSide"; 
-import SignUpSide from "../userAuth/ManagerSignUp/SignUpSide";
+// import SignInSide from "../userAuth/ManagerSignIn/ManagerSignInPage"; 
+// import SignUpSide from "../userAuth/ManagerSignUp/ManagerSignUpPage";
 
-const HomePage: React.FC = () => {
-  const { isLoggedIn, setIsLoggedIn, setRole } = useUserStore();
-  const [isNewUser, setIsNewUser] = useState(true);
+// const HomePage: React.FC = () => {
+//   const { isLoggedIn, setIsLoggedIn, setRole } = useUserStore();
+//   const [isNewUser, setIsNewUser] = useState(true);
   
 
-  useEffect(() => {
-    const unsubscribe = onAuthStateChanged(auth, (user: any) => {
-      if (user) {
-        setIsLoggedIn(true);
-      } else {
-        setIsLoggedIn(false);
-      }
-    });
-    return () => unsubscribe();
-  }, [setIsLoggedIn]);
+//   useEffect(() => {
+//     const unsubscribe = onAuthStateChanged(auth, (user: any) => {
+//       if (user) {
+//         setIsLoggedIn(true);
+//       } else {
+//         setIsLoggedIn(false);
+//       }
+//     });
+//     return () => unsubscribe();
+//   }, [setIsLoggedIn]);
 
-  return (
-    <Box>
-      {isLoggedIn ? (
-        <Paper>
-          <Typography variant="h6">You are logged in.</Typography>
-        </Paper>
-      ) : (
-        <Paper>
-          <Box sx={{ 
-            display: 'flex',
-            flexDirection: 'row',
-            gap: 1,
-            alignItems: 'center', 
-            width: "50%", 
-          }}>
-            <Button variant={isNewUser ? "contained" : "outlined"} onClick={() => setIsNewUser(true)} fullWidth>
-              New here? Sign up
-            </Button>
-            <Button variant={!isNewUser ? "contained" : "outlined"} onClick={() => setIsNewUser(false)} fullWidth>
-              Already a user? Log In
-            </Button>
-          </Box>
-          {isNewUser ? <SignUpSide /> : <SignInSide />} 
-        </Paper>
-      )}
-    </Box>
-  );
-};
+//   return (
+//     <Box>
+//       {isLoggedIn ? (
+//         <Paper>
+//           <Typography variant="h6">You are logged in.</Typography>
+//         </Paper>
+//       ) : (
+//         <Paper>
+//           <Box sx={{ 
+//             display: 'flex',
+//             flexDirection: 'row',
+//             gap: 1,
+//             alignItems: 'center', 
+//             width: "50%", 
+//           }}>
+//             <Button variant={isNewUser ? "contained" : "outlined"} onClick={() => setIsNewUser(true)} fullWidth>
+//               New here? Sign up
+//             </Button>
+//             <Button variant={!isNewUser ? "contained" : "outlined"} onClick={() => setIsNewUser(false)} fullWidth>
+//               Already a user? Log In
+//             </Button>
+//           </Box>
+//           {isNewUser ? <SignUpSide /> : <SignInSide />} 
+//         </Paper>
+//       )}
+//     </Box>
+//   );
+// };
 
-export default HomePage;
+// export default HomePage;
