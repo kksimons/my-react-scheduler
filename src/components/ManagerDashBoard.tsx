3c616daa919284f2380ad330fe11a37ac7bde676
Vy Truong <trmninhvy@gmail.com>
2024-10-16 01:18:38 -0600

    c7 be able to run the scheduler
    https://github.com/Bitnoise/react-scheduler

--- src/components/ManagerDashBoard.tsx
+++ src/components/ManagerDashBoard.tsx
@@ -4,13 +4,14 @@ import React from 'react';
 import { Container, Typography, Button, Box } from '@mui/material';
 import { styled } from '@mui/system';
 import SchedulerContainer from '../schedules/SchedulerContainer';
+import { Component } from '../schedules/bitnotScheduler';
 const ManagerDashBoard: React.FC = () => { 
     return (
         <Typography> 
             This Is Employee DashBoard: 
                 Available Page: 
                 <Box>
-                    <SchedulerContainer/>
+                    <Component />
                 </Box>
                
         </Typography>
